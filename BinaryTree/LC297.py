f784bd1caaea55aa109242830de4e1bc4810baf0
LinyiHan1998 <linyihan25@gmail.com>
2023-09-01 15:38:38 -0700

    0901

--- BinaryTree/LC297.py
+++ BinaryTree/LC297.py
@@ -1,26 +1,104 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from collections import deque
 class Codec:
-    def traverse(self,root,res):
-        if root is None:
-            res.append(str(None))
-            return 
-        res.append(str(root.val))
-        self.traverse(root.left,res)
-        self.traverse(root.right,res)
+    def __init__(self):
+        self.SEP = ','
+        self.EMPTY = '#'
+        self.res = ''
+    ####pre-order###
+    def preTraverse(self,root):
+        if root == None:
+            self.res += self.EMPTY+self.SEP
+            return
+        self.res += str(root.val) +self.SEP
+        self.preTraverse(root.left)
+        self.preTraverse(root.right)
 
-    def deseralize_helper(self,d):
-        if d == []:return None
-        root_val = d.pop(0)
-        if root_val == 'None':return None
+    def preTraverseDecode(self,nodes):
+        if nodes == []:
+            return None
+
+        root_val = nodes.pop(0)
+        if root_val == self.EMPTY:
+            return None
         root = TreeNode(root_val)
-        root.left = self.deseralize_helper(d)
-        root.right = self.deseralize_helper(d)
+        root.left = self.preTraverseDecode(nodes)
+        root.right = self.preTraverseDecode(nodes)
         return root
+    ###post-order###
+    def postTraverse(self,root):
+        if root == None:
+            self.res += self.EMPTY+self.SEP
+            return
         
+        self.postTraverse(root.left)
+        self.postTraverse(root.right)
+        self.res += str(root.val) +self.SEP
+
+    def postTraverseDecode(self,nodes):
+        if nodes == []:return None
+        root_val = nodes.pop()
+        if root_val == self.EMPTY:
+            return None
+        root = TreeNode(int(root_val))
+        root.right = self.postTraverseDecode(nodes)
+        root.left = self.postTraverseDecode(nodes)
+        return root
+
+    ###sequence traversal###
+    def iterativeTraverse(self,root):
+        if root == None: return None
+        queue=[root]
+        while queue:
+            sz = len(queue)
+            for i in range(sz):
+                cur = queue.pop(0)
+                if cur is None:
+                    self.res += self.EMPTY + self.SEP
+                    continue
+                self.res += str(cur.val) + self.SEP
+                queue.append(cur.left)
+                queue.append(cur.right)
+
+
+    def iterativeTraverseDecode(self,nodes):
+        if nodes == []: return None
+        root = TreeNode(nodes.pop(0))
+        queue = [root]
+        while queue:
+            sz = len(queue)
+            for i in range(sz):
+                parent = queue.pop(0)
+                if parent is None: continue
+                left = nodes.pop(0)
+                right = nodes.pop(0)
+
+                if left != self.EMPTY:
+                    parent.left = TreeNode(left)
+                    queue.append(parent.left)
+                
+                if right !=self.EMPTY:
+                    parent.right = TreeNode(right)
+                    queue.append(parent.right)
+
+        return root
+
     def serialize(self, root):
-        res = []
-        self.traverse(root,res)
-        return str(res)
-    
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        #self.preTraverse(root)
+        #self.postTraverse(root)
+        self.iterativeTraverse(root)
+        return self.res
+        
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -28,5 +106,16 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        d = ' '.join(eval(data)).split()
-        return self.deseralize_helper(d)
+        arr = data.split(self.SEP)
+        arr.remove('')
+        #if data == [] :return None
+        
+        #return self.preTraverseDecode(arr)
+        #return self.postTraverseDecode(arr)
+        return self.iterativeTraverseDecode(arr)
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
