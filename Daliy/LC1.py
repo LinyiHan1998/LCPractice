a4182e2e54e6d342198d5e9e9c232069132a47cf
LinyiHan1998 <linyihan25@gmail.com>
2023-10-15 21:27:57 -0700

    1015_2

---
+++ Daliy/LC1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i in range(len(nums)):
+            if nums[i] in dic:
+                return [i,dic[nums[i]]]
+            dic[target-nums[i]] = i
