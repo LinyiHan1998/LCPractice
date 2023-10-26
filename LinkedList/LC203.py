c3e3d0e12a5fa6941df827e0109ba810f495d859
LinyiHan1998 <linyihan25@gmail.com>
2023-10-26 15:47:41 -0700

    1026_9

---
+++ LinkedList/LC203.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummy = ListNode(0,head)
+        p = dummy
+        while p and p.next:
+            if p.next.val == val:
+                p.next = p.next.next
+            else:
+                p = p.next
+        return dummy.nexta
+
+        
