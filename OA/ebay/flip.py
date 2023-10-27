178149e81d96335951a252ce08681b764c4b1fd1
LinyiHan1998 <linyihan25@gmail.com>
2023-10-27 00:16:38 -0700

    ebay_fi

--- OA/ebay/flip.py
+++ OA/ebay/flip.py
@@ -1,34 +1,36 @@
-def flip(s):#带上函数名字抄，外面还要补个string处理
-    res = ''
-    for ch in s:
-        if ch == '0':
-            res += '1'
-        else:
-            res += '0'
-    return res
-def count(s,x):
-    res = 0
-    for i in range(x):
-        if s[i] == '0':
-            res += 1
-    return res
+
 def solution(bistr,requests):
     res = []
-    for request in requests:
-        if 'count' in request:
-            i = 6
-            x = ''
-            while request[i]!='>':
-                x += request[i]
-                i += 1
-            res.append(count(bistr,int(x)))
+    b1 = bistr
+    b2 = "".join([str(1-int(c)) for c in bistr])
+
+    if bistr[0] == '0':
+        c1,c2 = [0],[1]
+    else:
+        c1,c2 = [1],[0]
+    
+    for i in range(1,len(bistr)):
+        if bistr[i]=='1':
+            c1.append(c1[-1]+1)
+            c2.append(c2[-1])
         else:
-            res.append(flip(bistr))
+            c1.append(c1[-1])
+            c2.append(c2[-1]+1)
+
+    for request in requests:
+        s = request.split(":")
+        if len(s)==1:
+            bistr = b1 if bistr == b2 else b2
+        if len(s) == 2:
+            if bistr == b1:
+                res.append(c1[int(s[1])])
+            else:
+                res.append(c2[int(s[1])])
     return res
 
 if __name__=='__main__':
     bistr = '01010101111'
-    requests = ['count<3>','flip']
-    print(count(bistr,3))
-    print(flip(bistr))
+    requests = ['count:3','flip']
+    # print(count(bistr,3))
+    # print(flip(bistr))
     print(solution(bistr,requests))
