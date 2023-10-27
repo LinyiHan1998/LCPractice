f3c1737909e6f3ae93a172a96da172e6690af803
LinyiHan1998 <linyihan25@gmail.com>
2023-10-26 17:33:17 -0700

    1026_11

---
+++ LinkedList/LC206.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        pre = None
+        while cur:
+            tmp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = tmp
+        return pre
